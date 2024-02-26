434dbf78b55166dfc0049d343ea67299d0a7bd48
박지용 <gdragon0228@naver.com>
2024-02-26 18:27:06 +0900

    로그인 ui

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:pamtion_mobile/const/color.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -6,149 +8,150 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // backgroundColor: PRIMARY_COLOR,
       body: SafeArea(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Container(
-              height: MediaQuery.of(context).size.height,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(color: Color(0xFFFAFAFA)),
-              child: Center(
-                child: Stack(
-                  children: [
-                    Positioned(
-                      top: 20,
-                      left: 20,
-                      child: Text(
-                        '환영합니다!',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 12,
-                          fontWeight: FontWeight.w500,
-                        ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: 102,
+                  ),
+                  Container(
+                    width: 60,
+                    height: 60,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('asset/img/pamtion_icon.png'),
+                        fit: BoxFit.fill,
                       ),
                     ),
-                    Positioned(
-                      top: 60,
-                      left: 20,
-                      child: Container(
-                        width: 30,
-                        height: 30,
-                        decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          image: DecorationImage(
-                            image: NetworkImage(
-                                "https://via.placeholder.com/30x30"),
-                            fit: BoxFit.fill,
-                          ),
-                        ),
-                      ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Text(
+                    '환영합니다!',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w700,
+                      fontSize: 20,
+                      fontFamily: 'nanumsqare',
                     ),
-                    Positioned(
-                      top: 120,
-                      left: 20,
-                      child: Container(
-                        width: 295,
-                        height: 36,
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        width: MediaQuery.of(context).size.width - 80,
+                        height: 40,
                         decoration: BoxDecoration(
-                          color: Colors.white,
                           borderRadius: BorderRadius.circular(5),
-                          border: Border.all(color: Color(0xFFE8E8E8)),
+                          border: Border.all(color: Colors.black),
                         ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Container(
-                              width: 18,
-                              height: 18,
-                              decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                image: DecorationImage(
-                                  image: NetworkImage(
-                                      "https://via.placeholder.com/18x18"),
-                                  fit: BoxFit.fill,
+                            Row(
+                              children: [
+                                SizedBox(
+                                  width: 40,
                                 ),
-                              ),
-                            ),
-                            SizedBox(width: 10),
-                            Text(
-                              'Google로 계속하기',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 13,
-                              ),
+                                Image.asset('asset/img/google.png'),
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 20),
+                                  child: Text(
+                                    'Google로 계속하기',
+                                    style: TextStyle(
+                                      fontFamily: 'nanumsqare',
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                ),
+                              ],
                             ),
                           ],
                         ),
                       ),
-                    ),
-                    Positioned(
-                      top: 180,
-                      left: 20,
-                      child: Container(
-                        width: 295,
-                        height: 36,
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      Container(
+                        width: MediaQuery.of(context).size.width - 80,
+                        height: 40,
                         decoration: BoxDecoration(
-                          color: Colors.white,
                           borderRadius: BorderRadius.circular(5),
-                          border: Border.all(color: Color(0xFFE8E8E8)),
+                          border: Border.all(color: Colors.black),
                         ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            SizedBox(width: 5),
-                            Image(image: Image.asset(name)),
-                            SizedBox(width: 10),
-                            Text(
-                              'Github로 계속하기',
-                              style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 13,
-                              ),
+                            Row(
+                              children: [
+                                SizedBox(
+                                  width: 40,
+                                ),
+                                SizedBox(
+                                  width: 18,
+                                  height: 18,
+                                  child:
+                                      Image.asset('asset/img/githublogo.png'),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 20),
+                                  child: Text(
+                                    'Github로 계속하기',
+                                    style: TextStyle(
+                                      fontFamily: 'nanumsqare',
+                                      fontSize: 20,
+                                    ),
+                                  ),
+                                ),
+                              ],
                             ),
                           ],
                         ),
                       ),
-                    ),
-                    Positioned(
-                      top: 240,
-                      left: 20,
-                      child: Container(
-                        width: 295,
-                        height: 1,
-                        color: Color(0xFFD2D2D2),
-                      ),
-                    ),
-                    Positioned(
-                      top: 250,
-                      left: 20,
-                      child: Text(
+                    ],
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Divider(
+                    color: Colors.grey,
+                    height: 20,
+                  ),
+                  SizedBox(height: 40,),
+                  Column(
+                    children: [
+                      Text(
                         'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
                         style: TextStyle(
                           color: Color(0xFFAAAAAA),
                           fontSize: 10,
                         ),
                       ),
-                    ),
-                    Positioned(
-                      child: SizedBox(
-                        width: 257,
-                        child: Text(
-                          'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut',
-                          style: TextStyle(
-                            color: Color(0xFFAAAAAA),
-                            fontSize: 8,
-                            fontFamily: 'Pretendard',
-                            fontWeight: FontWeight.w400,
-                            height: 0,
-                          ),
+                      Text(
+                        'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut',
+                        style: TextStyle(
+                          color: Color(0xFFAAAAAA),
+                          fontSize: 8,
+                          fontFamily: 'Pretendard',
+                          fontWeight: FontWeight.w400,
+                          height: 0,
                         ),
                       ),
-                    ),
-                  ],
-                ),
+                    ],
+                  ),
+                ],
               ),
             )
           ],
